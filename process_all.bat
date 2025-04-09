948636f52dc472f567ee22fef81140cfe461d26f
George <g.e.tziolas@gmail.com>
2025-04-09 15:47:44 +0300

    Delete old files

--- process_all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for %%f in (C:\Users\gigak\PycharmProjects\realm_task_3_3_xai_uc2\Preprocessed\*.preprocessed.vcf) do (
-  echo Processing %%f
-  docker run --rm -v C:/Users/gigak/PycharmProjects/realm_task_3_3_xai_uc2/Preprocessed/:/pharmcat/data pgkb/pharmcat pharmcat_pipeline --missing-to-ref -matcher -phenotyper -reporterJson /pharmcat/data/%%~nxf
-)
